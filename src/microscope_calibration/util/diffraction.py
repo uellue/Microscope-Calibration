Fix twotheta calculation
The large excitation error included weird duplicates

Also, consider that diffsims calculates theta, not twotheta

Add test case to check against literature values

src/microscope_calibration/util/diffraction.py
@@ -2,6 +2,7 @@ import numpy as np
 from CifFile import ReadCif
 from diffpy.structure import loadStructure
 from diffsims.generators.simulation_generator import SimulationGenerator
+from diffsims.generators.zap_map_generator import get_rotation_from_z_to_direction
 from orix.crystal_map import Phase
 from orix.quaternion import Rotation
 
@@ -11,27 +12,33 @@ from orix.quaternion import Rotation
 def get_twothetas(cif_filename, acceleration_voltage_V, reciprocal_radius=1):
     gen = SimulationGenerator(
         accelerating_voltage=acceleration_voltage_V / 1000,
+        precession_angle=10,
+        minimum_intensity=0.0001,
     )
     structure_raw = ReadCif(cif_filename)
     key = list(structure_raw.keys())[0]
     space_group = int(structure_raw[key]["_space_group_IT_number"])
     structure = loadStructure(cif_filename)
     p = Phase(structure=structure, space_group=space_group)
-    twothetas = set()
-    rng = np.random.default_rng(seed=0)
-    eulers = rng.uniform(0.0, 2 * np.pi, (10, 3))
-    for euler in eulers:
-        rot = Rotation.from_euler(euler)
-        sim = gen.calculate_diffraction2d(
-            phase=p,
-            rotation=rot,
-            reciprocal_radius=reciprocal_radius,
-            # Large excitation error to capture many peaks
-            max_excitation_error=1,
-        )
-        sim.coordinates.calculate_theta(voltage=acceleration_voltage_V)
-        thetas_with_intensity = [
-            item[1] for item in zip(sim.coordinates.intensity, sim.coordinates.theta) if item[0] > 1
-        ]
-        twothetas.update(np.round(thetas_with_intensity, decimals=5))
-    return np.array(sorted(twothetas))
+    thetas = set()
+    for ha in (-1, 0, 1,):
+        for ka in (-1, 0, 1,):
+            for el in (-1, 0, 1):
+                euler = get_rotation_from_z_to_direction(p.structure, [ha, ka, el])
+                rot = Rotation.from_euler(euler)
+                sim = gen.calculate_diffraction2d(
+                    phase=p,
+                    rotation=rot,
+                    reciprocal_radius=reciprocal_radius,
+                    # This seems to avoid errorneous reflections while still including enough peaks
+                    max_excitation_error=0.0001,
+                )
+                sim.coordinates.calculate_theta(voltage=acceleration_voltage_V)
+                thetas_with_intensity = [
+                    item[1]
+                    for item in zip(sim.coordinates.intensity, sim.coordinates.theta)
+                    if item[0] > 1
+                ]
+                thetas.update(np.round(thetas_with_intensity, decimals=5))
+    # diffsims calculates theta, not twotheta
+    return np.array(sorted(thetas)) * 2